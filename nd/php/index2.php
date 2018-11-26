328f2573be2d2ca50b123d68af7507b17d84cd71
astute787 <asta.peskovaja89@yahoo.com>
2018-11-26 20:32:37 +0200

    pirmos dienos php uzduotys

---
+++ nd/php/index2.php
@@ -0,0 +1,18 @@
+<?php
+$country = 'Lietuva';
+$city = 'Vilnius';
+$largest_population = true;
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+    <p><?php echo $city.'- Lietuvos sostinė'; ?></p>
+    <p><?php echo "$city ir $country sparčiai auga"; ?></p>
+    
+   </body>
+ </html>
