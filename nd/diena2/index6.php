61675921946826fb34048845f61035c64ac8a632
astute787 <asta.peskovaja89@yahoo.com>
2018-12-03 12:45:04 +0200

    pataisytas namu darbas php 9

--- nd/diena2/index6.php
+++ nd/diena2/index6.php
@@ -2,7 +2,7 @@
           $ceu = array( "Italija"=>"Roma", "Liuksemburgas"=>"Liuksemburgas", "Belgija"=> "Briuselis", "Danija"=>"Kopenhaga", "Suomija"=>"Helsinkis", "Prancūzija" => "Paryžius", "Slovakija"=>"Bratislava", "Slovenija"=>"Liublijana", "Vokietija" => "Berlynas", "Graikija"=>"Atėnai", "Airija"=>"Dublinas", "Nyderlandai"=>"Amsterdamas", "Portugalija"=>"Lisabona", "Ispanija"=>"Madridas", "Švedija"=>"Stokholmas", "Didžioji Britanija"=>"Londonas", "Kipras"=>"Nikosija", "Lietuva"=>"Vilnius", "Čekija"=>"Praha", "Estija"=>"Talinas", "Vengrija"=>"Budapeštas", "Latvija"=>"Ryga", "Malta"=>"Valeta", "Austrija" => "Viena", "Bulgarija" => "Sofija", "Rumunija" => "Bukareštas", "Lenkija"=>"Varšuva");
 
           asort($ceu);
-          foreach($ceu as $miestas => $sostine){
-              echo 'Valstybė: ' . $miestas . ', Sostinė: ' . $sostine . '<br />';
+          foreach($ceu as $valstybe => $sostine){
+              echo 'Valstybė: ' . $valstybe . ', Sostinė: ' . $sostine . '<br />';
           }
       ?>
